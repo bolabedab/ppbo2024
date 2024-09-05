a3f2bf3118f46cbe8bdd089ca5c7beb6757fb2bf
Muhammad Roihan <moshing12gt@gmail.com>
2024-09-05 19:37:56 +0700

    #

--- pertama.php
+++ pertama.php
@@ -2,14 +2,5 @@
 
 date_default_timezone_set("Asia/Jakarta");
 $nama = readline('Masukkan nama Anda: ');
-$jam = date('H');
-if ($jam >=3 && $jam < 11){
-    $waktu = "Selamat pagi";
-} else if ($jam >= 11 && $jam < 15){
-    $waktu = "Selamat Siang";
-} else if ($jam >= 15 && $jam < 18){
-    $waktu = "Selamat Sore";
-} else {
-    $waktu = "Selamat malam";
-}
-echo "{$waktu}, {$nama} sekarang pukul {$jam}\n";
+$waktu = date('H');
+echo "Halo {$nama} sekarang pukul {$waktu}\n";
