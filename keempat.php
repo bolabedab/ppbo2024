5a8fe5944e110e20f38b56c2bb861a3abf3875b0
Muhammad Roihan <moshing12gt@gmail.com>
2024-09-06 15:59:09 +0700

    Praktikum 5E - Menggunakan Require Once untuk Memasukkan File yang Berisi Class

--- keempat.php
+++ keempat.php
@@ -1,103 +1,13 @@
 <?php
 
-class Author {
-    public $name;
-    public $description;
-
-    public function __construct($name, $description)
-    {
-        $this->name = $name;
-        $this->description = $description;
-    }
-
-    public function show($type): array
-    {
-        if ($type == "name") {
-            return ['name' => $this->name];
-        } elseif ($type == "description") {
-            return ['description' => $this->description];
-        } else {
-            return ['name' => $this->name, 'description' => $this->description];
-        }
-    }
-}
-
-
-class Publisher
-{
-    public $name;
-    public $address;
-    private $phone;
-
-    public function __construct($name, $address)
-    {
-        $this->name = $name;
-        $this->address = $address;
-    }
-
-    public function setPhone($phone): void
-    {
-        $this->phone = $phone;
-    }
-
-
-    public function getPhone(): string
-    {
-        return $this->phone;
-    }
-}
-
-class Book
-{
-    public $ISBN;
-    public $title;
-    public $description;
-    public $category;
-    public $language;
-    public $numberOfPage;
-    public $author;
-    public $publisher;
-
-    public function __construct($ISBN, $title, $description, $category, $language, $numberOfPage, $author, $publisher)
-    {
-        $this->ISBN = $ISBN;
-        $this->title = $title;
-        $this->description = $description;
-        $this->category = $category;
-        $this->language = $language;
-        $this->numberOfPage = $numberOfPage;
-        $this->author = $author;
-        $this->publisher = $publisher;
-    }
-
-    public function showAll(): array
-    {
-        return [
-            'ISBN' => $this->ISBN,
-            'title' => $this->title,
-            'description' => $this->description,
-            'category' => $this->category,
-            'language' => $this->language,
-            'numberOfPage' => $this->numberOfPage,
-            'author' => $this->author,
-            'publisher' => $this->publisher,
-        ];
-    }
-
-    public function detail($ISBN): array
-    {
-        if ($this->ISBN == $ISBN) {
-            return $this->showAll();
-        } else {
-            return ['error' => 'ISBN tidak ditemukan'];
-        }
-    }
-}
+require_once 'Author.php';
+require_once 'Publisher.php';
+require_once 'Book.php';
 
 $author = new Author("Eichiro Oda", "Penulis dari One Piece");
-$publisher = new Publisher("Jepang", "50 Bedford Square, Jepang");
+$publisher = new Publisher("Jepang", "Onigashima, Jepang");
 $publisher->setPhone("123456789");
-$book = new Book(9780747532743, "Harry Potter and the Philosopher's Stone", "Novel fantasi", "Fantasy", "English", 223, $author->name, $publisher->name);
+$book = new Book(9780747532743, "One Piece Volume 97", "Komik", "Adventure", "Jepang", 200, $author->name, $publisher->name);
 
 echo "Informasi Buku:\n";
 print_r($book->showAll());
