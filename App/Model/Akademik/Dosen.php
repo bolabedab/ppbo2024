9ea8eee9b49ef17a72fcd57a5ec9bf3c7bb2855a
Muhammad Roihan <moshing12gt@gmail.com>
2024-09-28 11:13:08 +0700

    Praktikum 6Cx - Implementasi Enkapsulasi

--- App/Model/Akademik/Dosen.php
+++ App/Model/Akademik/Dosen.php
@@ -5,13 +5,14 @@ namespace App\Model\Akademik;
 use App\Model\Akademik\Pegawai;
 
 class Dosen extends Pegawai{
-    public $nidn;
+    private $nidn;
 
-    public function __construct(int $nip, string $nama, int $no_hp, string $alamat, int $nidn) {
-        parent::__construct($nip,$nama,$no_hp,$alamat);
+    public function __construct(int $nip, string $nama, int $no_hp, string $alamat, string $nidn)
+    {
+        parent::__construct($nip, $nama, $no_hp, $alamat);
         $this->nidn = $nidn;
     }
-
+    
     public function mengajar(): void {
         echo "{$this->nama} sedang mengajar perkuliahan\n";
     }
